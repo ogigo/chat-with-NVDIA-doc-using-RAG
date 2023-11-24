c8ced8f11e965b2750184ba652a2e9c1caab2e96
ogigo <shamimahamed190@gmail.com>
2023-11-25 05:19:16 +0600

    add chain

---
+++ chain.py
@@ -0,0 +1,37 @@
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+from embedding import instructor_embeddings
+from llm import llm
+
+vectordb_file_path = "faiss_index"
+
+
+
+
+def get_qa_chain():
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
+
+    retriever = vectordb.as_retriever(score_threshold=0.7)
+
+    prompt_template = """Given the following context and a question, generate an answer based on this context only.
+    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+    CONTEXT: {context}
+
+    QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type="stuff",
+                                        retriever=retriever,
+                                        input_key="query",
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt": PROMPT})
+
+    return chain
